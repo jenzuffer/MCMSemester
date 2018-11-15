8f7d66efc4272b95d2fd7124409c9bc026e85b39
Mark <mark.d.denner@gmail.com>
2018-11-15 11:17:52 +0100

    ændret metode navne

--- src/main/java/FunctionLayer/LogicFacade.java
+++ src/main/java/FunctionLayer/LogicFacade.java
@@ -50,7 +50,7 @@ public class LogicFacade {
     }
     
     
-    public static CarportDimensioner test (int length, int width, boolean tag) throws LoginSampleException {
+    public static CarportDimensioner calculateCarportList (int length, int width, boolean tag) throws LoginSampleException {
         Calculator calc = new Calculator();
         return calc.calculate(length, width, tag);
     }
--- src/main/java/PresentationLayer/CalculateOrder.java
+++ src/main/java/PresentationLayer/CalculateOrder.java
@@ -29,7 +29,7 @@ public class CalculateOrder extends Command {
         int width = Integer.valueOf(request.getParameter("width")) == null ? 0 : Integer.valueOf(request.getParameter("width"));
         int length = Integer.valueOf(request.getParameter("length")) == null ? 0 : Integer.valueOf(request.getParameter("length"));
         int indexID = 1; //tilføj måde til at sige hvilken order vi taler om
-        CarportDimensioner test = LogicFacade.test(length, width, true);
+        CarportDimensioner test = LogicFacade.calculateCarportList(length, width, true);
         //OrderMaterials = LogicFacade.calculateOrder(dimension, indexID);
         request.setAttribute("carport", test);
        // session.setAttribute("ordermaterials", OrderMaterials);
