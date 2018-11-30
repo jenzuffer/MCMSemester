109f246464c8632352909d4964938bb6a40744de
Mads Wulff Nielsen <mwn@Madss-MacBook-Pro.local>
2018-11-30 13:28:53 +0100

    pdf insert

--- src/main/java/PresentationLayer/PDFRenderer.java
+++ src/main/java/PresentationLayer/PDFRenderer.java
@@ -9,6 +9,9 @@ import FunctionLayer.Carport;
 import FunctionLayer.LogicFacade;
 import FunctionLayer.LoginSampleException;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -21,13 +24,18 @@ public class PDFRenderer extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
         Carport carport = (Carport) request.getSession().getAttribute("carport");
-        PDFGenerator pdf = new PDFGenerator(carport);
+        PDFGenerator pdfGen = new PDFGenerator(carport);
         try {
-            LogicFacade.inserPdf(1, pdf.generatePdf());
+            byte[] pdf = pdfGen.generatePdf();
+            LogicFacade.inserPdf(1, pdf);
+            request.setAttribute("pdf", pdf);
+            request.getRequestDispatcher("/PDF").forward(request, response);
         } catch (IOException ex) {
             System.out.println("Failed");
+        } catch (ServletException ex) {
+            System.out.println("Servlet exception");
         }
-        return "renderpdf";
+        return "itemlist";
     }
 
 }
