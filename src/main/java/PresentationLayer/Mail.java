7f16c0a9675da159db9bb8ca5ab1829794cf087b
Mads Wulff Nielsen <mwn@Madss-MacBook-Pro.local>
2018-12-03 10:04:24 +0100

    email with attachtment implemented - needs to be implemented in other way, but its working

---
+++ src/main/java/PresentationLayer/Mail.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PresentationLayer;
+
+import FunctionLayer.User;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.commons.mail.ByteArrayDataSource;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.MultiPartEmail;
+
+/**
+ *
+ * @author mwn
+ */
+public class Mail {
+
+    private final String HOST_NAME = "smtp.googlemail.com";
+    private final int SMTP_PORT = 465;
+    private final String SENDER_EMAIL = "mcmfog@gmail.com";
+    private final String SENDER_PASS = "Fog123456";
+
+    private User user;
+    private byte[] pdf;
+
+    public Mail(User user, byte[] pdf) {
+        this.user = user;
+        this.pdf = pdf;
+    }
+
+    public void sendEmailWithAttachtment() throws EmailException, IOException {
+        MultiPartEmail email = new MultiPartEmail();
+        email.setHostName(HOST_NAME);
+        email.setSmtpPort(SMTP_PORT);
+        email.setAuthenticator(new DefaultAuthenticator(SENDER_EMAIL, SENDER_PASS));
+        email.setSSLOnConnect(true);
+        email.addTo(user.getEmail(), user.getName());
+        email.setFrom(SENDER_EMAIL, "MCM FogSemesterProjekt");
+        email.setSubject("Carport stykliste");
+        email.setMsg("Her er en pdf, hvor du kan se din bestilte carport");
+        email.attach(new ByteArrayDataSource(pdf, "application/pdf"), "stykliste.pdf", "Description of some file");
+        email.send();
+    }
+}
--- src/main/java/PresentationLayer/PDFRenderer.java
+++ src/main/java/PresentationLayer/PDFRenderer.java
@@ -8,12 +8,14 @@ package PresentationLayer;
 import FunctionLayer.Carport;
 import FunctionLayer.LogicFacade;
 import FunctionLayer.LoginSampleException;
+import FunctionLayer.User;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.mail.EmailException;
 
 /**
  *
@@ -30,10 +32,13 @@ public class PDFRenderer extends Command {
             LogicFacade.inserPdf(1, pdf);
             request.setAttribute("pdf", pdf);
             request.getRequestDispatcher("/PDF").forward(request, response);
+            new Mail(new User("Mads", null, null, null, "cph-mn492@cphbusiness.dk"), pdf).sendEmailWithAttachtment();
         } catch (IOException ex) {
             System.out.println("Failed");
         } catch (ServletException ex) {
             System.out.println("Servlet exception");
+        } catch (EmailException ex) {
+            System.out.println("Email failed");
         }
         return "itemlist";
     }
